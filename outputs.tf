9ecd8a333d077efea806a5974bdc85b2419c1b20
adamwshero <cloudarmy.io@gmail.com>
2022-09-28 15:58:00 -0600

    .

--- outputs.tf
+++ outputs.tf
@@ -16,16 +16,12 @@ output "replica_kms_key_id" {
   description = "The key ID of the replica key. Related multi-Region keys have the same key ID."
   value       = var.replica_is_enabled ? aws_kms_replica_key.this[0].arn : "[INFO] Replica KMS Key Skipped."
 }
-output "kms_primary_sops_file" {
+output "primary_kms_sops_file" {
   description = "Output of the newly created KMS SOPS file."
-  value       = var.enable_sops_primary ? local_file.sops_primary[0].content : "[INFO] Replica KMS Key Skipped."
+  value       = var.enable_sops_primary ? local_file.sops_primary[0].content : "[INFO] Primary SOPS Skipped."
 }
 
-output "kms_replica_sops_file" {
+output "replica_kms_sops_file" {
   description = "Output of the newly created KMS SOPS file."
-  value       = <<EOF
----
-creation_rules:
-  - kms: ${aws_kms_replica_key.this[0].arn}
-EOF
+  value       = var.enable_sops_replica ? local_file.sops_replica[0].content : "[INFO] Replica SOPS Skipped."
 }

104eeddd8d60fcbdf5ea5a66c8b657b0a443c185
Adam Shero <cloudarmy.io@gmail.com>
2024-10-11 18:42:24 -0600

    Add grant functionality (#6)

--- examples/terraform/main.tf
+++ examples/terraform/main.tf
@@ -7,7 +7,7 @@ data "aws_iam_roles" "roles" {
 }
 
 module "primary-kms-sops" {
-  source = "git@github.com:adamwshero/terraform-aws-kms.git//.?ref=1.1.6"
+  source = "git@github.com:adamwshero/terraform-aws-kms.git//.?ref=1.2.0"
 
   is_enabled                         = true
   name                               = "alias/devops"
@@ -26,11 +26,24 @@ module "primary-kms-sops" {
 
   // SOPS Config
   enable_sops_primary = true
-  sops_file           = "${get_terragrunt_dir()}/.sops.yaml"
+  sops_file           = "${path.module}/.sops.yaml"
+
+  // Grant Config
+  grant_is_enabled      = true
+  grant_name            = "test-grant"
+  grantee_principal     = local.env_vars.locals.sso_administrator_role_arn
+  retiring_principal    = local.env_vars.locals.sso_administrator_role_arn
+  operations            = ["Encrypt", "Decrypt", "GenerateDataKey"]
+  grant_creation_tokens = [base64encode("Token 1"), base64encode("Token 2")]
+  retire_on_delete      = true
+
+  encryption_context_equals = {
+    Department = "Platform Engineering"
+  }
 
   tags = {
     Environment        = local.env
-    Owner              = "DevOps"
+    Owner              = "Platform Engineering"
     CreatedByTerraform = true
   }
 }
